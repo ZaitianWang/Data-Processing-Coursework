07a2c03ba2c3e09296334454f9feb83c902a6f46
Zaitian <33757895+ZaitianWang@users.noreply.github.com>
2022-06-13 17:57:28 +0800

    add test query

---
+++ Chap3/do_query.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import boolean_model
+import vector_model
+
+
+# In[2]:
+
+
+bool_query("to")
+
+
+# In[3]:
+
+
+bool_query("do")
+
+
+# In[4]:
+
+
+bool_query("to do")
+
+
+# In[5]:
+
+
+bool_query("I am")
+
+
+# In[6]:
+
+
+bool_query("Let it")
+
+
+# In[7]:
+
+
+vect_query("to")
+
+
+# In[8]:
+
+
+vect_query("do")
+
+
+# In[9]:
+
+
+vect_query("to do")
+
+
+# In[10]:
+
+
+vect_query("I am")
+
+
+# In[11]:
+
+
+vect_query("Let it")
+
